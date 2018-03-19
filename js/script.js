7084274eddec273855a417a22ecb606559550197
Sukrit Goel <Sukritgoel@users.noreply.github.com>
2018-03-20 03:54:24 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -338,6 +338,7 @@ function respond(val) {
             //msg.voice = voicelist[1];
             //};
             // msg.voice = voicelist[1];
+            msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == voicelist[1]; })[0];
             window.speechSynthesis.speak(msg);
                         
             //check = false;
