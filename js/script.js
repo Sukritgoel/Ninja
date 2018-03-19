a262efecbaf840856c70ec9efe3b79361c3b9769
Sukrit Goel <Sukritgoel@users.noreply.github.com>
2018-03-20 00:45:44 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -318,7 +318,7 @@ function respond(val) {
                   msg.text = val;
             };*/
             msg.text = val;
-            msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == 'Microsoft Zira Desktop - English (United States)'; })[0];
+            msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == ''; })[0];
             window.speechSynthesis.speak(msg);
                         
             //check = false;
